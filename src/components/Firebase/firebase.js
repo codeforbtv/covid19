f4a4ad04d56db156ff67cbc22e0857f927397071
jcarevature <Justin.Adams@uvm.edu>
2020-09-12 16:28:58 -0400

    Updating application with Firebase integration.

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,20 @@
+import app from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCbRIf0Ek1brQMBH0BlH8BBBTHSaEKh9ao",
+    authDomain: "vt-covid-resources.firebaseapp.com",
+    databaseURL: "https://vt-covid-resources.firebaseio.com",
+    projectId: "vt-covid-resources",
+    storageBucket: "vt-covid-resources.appspot.com",
+    messagingSenderId: "857078712904",
+    appId: "1:857078712904:web:4e4781e6be53991205267d",
+    measurementId: "G-J9XKK8Q6MW",
+};
+
+class Firebase {
+    constructor() {
+       app.initializeApp(config); 
+   }
+}
+
+export default Firebase;
