798c68e57003466ade55f5311463a7c9e9cf7952
ejolly <eshin.jolly@gmail.com>
2020-09-12 18:37:52 -0400

    Overhaul with separate components for each database view filtering by health topics.

---
+++ src/components/CrisisHelpLines/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FirestoreCollection } from 'react-firestore';
+
+export default () => (
+    <FirestoreCollection
+        path={'health-wellness-accessibility'}
+        filter={[ 'topic', '==', 'Crisis Help Lines']}
+        render={({ data }) => {
+            return (
+                <div>
+                    <h1>Health</h1>
+                    <ul>
+                        {data.map(doc => (
+                            <li key={doc.id}>
+                                <strong>Name: </strong>{doc.name}
+                                <br />
+                                <strong>Topic: </strong>{doc.topic}
+                                <br />
+                                <strong>Number: </strong>{doc.number}
+                                <br />
+                                <strong>Website: </strong>{doc.website}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }}
+    />
+)
+
