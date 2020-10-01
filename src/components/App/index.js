00a7c333c76f260964097bb23116a9e6668cd4e0
Gabe Sequeira-Bacher <gxs11260@vsc.edu>
2020-10-01 18:24:25 -0400

    created a skeletal signup form, added App.css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import {
   BrowserRouter as Router, Route, Switch, Link
 } from 'react-router-dom';
@@ -7,8 +7,10 @@ import DomesticViolence from '../DomesticViolence';
 import ChildFamily from '../ChildFamily';
 import MentalHealth from '../MentalHealth';
 import CrisisHelpLines from '../CrisisHelpLines';
+import './App.css';
 
 const App = () => (
+  <>
   <Router>
     <div>
       <h1>Choose a Health Topic</h1>
@@ -44,6 +46,75 @@ const App = () => (
       </Switch>
     </div>
     </Router>
+    <Form/>
+    </>
 );
+
+const formReducer = (state, event) => {
+  if(event.reset) {
+    return {
+      name: '',
+      email: '',
+    }
+  }
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
+
+function Form() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+  
+    setTimeout(() => {
+      setSubmitting(false);
+      setFormData({
+        reset: true
+      })
+    }, 3000)
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    })
+  }
+
+  return(
+  <div className='wrapper'>
+  <h1>Sign Up for Our Email Newsletter!</h1>
+  {submitting &&
+  <div>You are submitting the following:
+    <ul>
+      {Object.entries(formData).map(([name, value]) => (
+        <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+      ))}
+    </ul>
+  </div>
+  }
+  <form onSubmit={handleSubmit}>
+    <fieldset disabled={submitting}>
+      <label>
+        <p>Name</p>
+        <input name='name' onChange={handleChange} value={formData.name}/>
+      </label>
+    </fieldset>
+    <fieldset disabled={submitting}>
+      <label>
+        <p>Email</p>
+        <input type='email' name='email' onChange={handleChange} value={formData.email}/>
+      </label>
+    </fieldset>
+    <button type='submit' disabled={submitting}>Submit</button>
+  </form>
+  </div>
+  )
+}
 
 export default App;
